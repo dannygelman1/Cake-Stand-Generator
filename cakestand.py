f942e28e97e8ccbdb3db0025f91344af8d7e5743
Danny Gelman <dannygelman1@gmail.com>
2020-09-29 18:35:14 -0400

    put in quad cylinder code

--- cakestand.py
+++ cakestand.py
@@ -1,43 +1,122 @@
 from functools import partial
 
 #Window
-cmds.window("Cake Stand", sizeable=True, resizeToFitChildren=True) 
+cmds.window("Quad Cylinder", sizeable=True, resizeToFitChildren=True) 
 cmds.columnLayout( adjustableColumn=True )                                             
 
 #Text
 cmds.separator(h=20)
-cmds.text("Adjust Parameters of the Cake Stand")
+cmds.text("Adjust Parameters of the Quad Cylinder")
 cmds.separator(h=20)
 
 #Stem Funcations
-def adjustBaseHeight(sliderHeight, *args, **kwargs):
+def adjustHeight(sliderHeight, *args, **kwargs):
     """
-    sliderHeight: floatSliderGrp object holding the stem radius value
+    sliderHeight: floatSliderGrp object holding the cylinder height value
         
-    Adjusts the stem radius of the stem based on the slider value
+    Adjusts the cylinder height based on the slider value
     """
     
-    valHeight = cmds.floatSliderGrp(sliderRadius, q=True, value=True)
-    basels = cmds.ls('base*', long=True)
-    baseName = basels[len(basels)-1][1:5]
-    baseNumber= basels[len(basels)-1][4]
-    cmds.select(baseName, r=True)
-    cmds.setAttr('polySphere' + baseNumber+ '.radius', valHeight, **kwargs)   
+    valHeight = cmds.floatSliderGrp(sliderHeight, q=True, value=True)
+    quadcylinderName = nameObject()
+    quadcylinderNumber = numberObject()
+    cmds.select(quadcylinderName, r=True)
+    cmds.setAttr('polyCylinder' + quadcylinderNumber + '.height', valHeight, **kwargs) 
+    
+def adjustRadius(sliderRadius, *args, **kwargs):
+    """
+    sliderRadius: floatSliderGrp object holding the cylinder radius value
+        
+    Adjusts the cylinder radius based on the slider value
+    """
+    
+    valRadius = cmds.floatSliderGrp(sliderRadius, q=True, value=True)
+    quadcylinderName = nameObject()
+    quadcylinderNumber = numberObject()
+    cmds.select(quadcylinderName, r=True)
+    cmds.setAttr('polyCylinder' + quadcylinderNumber+ '.radius', valRadius, **kwargs) 
+    
+def adjustSubH(sliderSubH, *args, **kwargs):
+    """
+    sliderSubH: floatSliderGrp object holding the cylinder subdivision height value
+        
+    Adjusts the cylinder subdivision height based on the slider value
+    """
+    quadcylinderName = nameObject()
+    cmds.delete(quadcylinderName)
+    base()
+    
+def adjustCap(sliderCap, *args, **kwargs):
+    """
+    sliderCap: floatSliderGrp object holding the cylinder subdivision cap value
+        
+    Adjusts the cylinder subdivision cap based on the slider value
+    """
+    quadcylinderName = nameObject()
+    cmds.delete(quadcylinderName)
+    base()     
+    
+def adjustSubAx(sliderSubAx, *args, **kwargs):
+    """
+    sliderSubAx: floatSliderGrp object holding the cylinder subdivision axis value
+        
+    Adjusts the cylinder subdivision axis based on the slider value
+    """
+    quadcylinderName = nameObject()
+    cmds.delete(quadcylinderName)
+    base() 
+      
+def nameObject():
+    quadcylinderls = cmds.ls('quad*', long=True)
+    quadcylinderNumber= str(len(quadcylinderls)/2)
+    quadcylinderName = 'quad' + quadcylinderNumber
+    return quadcylinderName
+    
+def numberObject():
+    quadcylinderls = cmds.ls('quad*', long=True)
+    quadcylinderNumber= str(len(quadcylinderls)/2)
+    return quadcylinderNumber
+    
 
-baseHeightSlider = cmds.floatSliderGrp(label='Base Height', columnAlign= (1,'right'), field=True, min=1, max=5, value=0, step=0.1, dc = 'empty')
-cmds.floatSliderGrp(baseHeightSlider,  e=True, dc = partial(adjustBaseHeight, baseHeightSlider))
+HeightSlider = cmds.floatSliderGrp(label='Height', columnAlign= (1,'right'), field=True, min=1, max=20, value=0, step=0.1, dc = 'empty')
+cmds.floatSliderGrp(HeightSlider,  e=True, dc = partial(adjustHeight, HeightSlider))
 
-#Butto'
-cmds.button(l = 'Create Cake Stand',  c = 'base()')
+RadiusSlider = cmds.floatSliderGrp(label='Radius', columnAlign= (1,'right'), field=True, min=1, max=20, value=0, step=0.1, dc = 'empty')
+cmds.floatSliderGrp(RadiusSlider,  e=True, dc = partial(adjustRadius, RadiusSlider))
+
+SubHSlider = cmds.intSliderGrp(label='Subdivision Height', columnAlign= (1,'right'), field=True, min=1, max=20, value=0, step=0.1, dc = 'empty')
+cmds.intSliderGrp(SubHSlider,  e=True, dc = partial(adjustSubH, SubHSlider))
+
+CapSlider = cmds.intSliderGrp(label='Subdivision Cap', columnAlign= (1,'right'), field=True, min=2, max=20, value=0, step=0.1, dc = 'empty')
+cmds.intSliderGrp(CapSlider,  e=True, dc = partial(adjustCap, CapSlider))
+
+SubAxSlider = cmds.intSliderGrp(label='Subdivision Axis', columnAlign= (1,'right'), field=True, min=6, max=20, value=0, step=0.1, dc = 'empty')
+cmds.intSliderGrp(SubAxSlider,  e=True, dc = partial(adjustSubAx, SubAxSlider))
+
+
+#Button
+cmds.button(l = 'Create Quad Cylinder',  c = 'base()')
 cmds.separator(h=20)
 cmds.showWindow()
 
     
 def base():
-    baseSides = 5
-    baseHieght = cmds.floatSliderGrp(baseHeightSlider, q=True, value=True)
-    baseSub = 3
-    baseRadius = 4
-    #baseShape = Circle
-   # finalCap = cmds.polySphere(n='cap#', r=3, sx=3, sy=3, ch=True)
-    finalStem = cmds.polyDisc(poly='disk#', radius=1, s=4, sd=3)
+    radius= cmds.floatSliderGrp(RadiusSlider, q=True, value=True)
+    height = cmds.floatSliderGrp(HeightSlider, q=True, value=True)
+    subax = cmds.intSliderGrp(SubAxSlider, q=True, value=True)
+    subheight = cmds.intSliderGrp(SubHSlider, q=True, value=True)
+    subcap = cmds.intSliderGrp(CapSlider, q=True, value=True)
+    #quaded cylinders can only have an even subdivision axis number, so this ensures that it will only be set to an even number
+    if (subax%2 == 1):
+        subax+=1
+    totaledges = ((subcap*4)+((subheight*2)-1))*subax
+    startEdgeToDelete = totaledges - subax*2
+    cylinder = cmds.polyCylinder(n='quad#', r=radius, h=height, sx=subax, sy=subheight, sc=subcap)
+    quadcylinderls = cmds.ls('quad*', long=True)
+    quadcylinderNumber= str(len(quadcylinderls)/2)
+    
+    #deleting every other edge in the inner most circle of the cylinder to turn the inner triangles into quads
+    listtodel = []
+    for i in range (startEdgeToDelete,totaledges,2):
+        listtodel.append('quad' + str(quadcylinderNumber) + '.e[' + str(i) + ']')
+    cmds.delete(listtodel)
